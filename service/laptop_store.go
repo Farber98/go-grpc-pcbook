bb22f8cc6ad7f1c9ad1ebe9735811677afe7a337
Farber98 <juantuc98@gmail.com>
2022-05-28 18:42:23 -0300

    implementing server-side streaming logic.

--- service/laptop_store.go
+++ service/laptop_store.go
@@ -16,7 +16,10 @@ var (
 type LaptopStore interface {
 	// Saves laptop to the store
 	Save(laptop *pb.Laptop) error
+	// Finds laptop in the store
 	Find(id string) (*pb.Laptop, error)
+	// Filters laptops from the store. Returns one by one via found func.
+	Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 type MemoryLaptopStore struct {
@@ -40,10 +43,9 @@ func (m *MemoryLaptopStore) Save(laptop *pb.Laptop) error {
 	}
 
 	//deep copy so it can't be modified by its pointer from external world inside our storage.
-	other := &pb.Laptop{}
-	err := copier.Copy(other, laptop)
+	other, err := deepCopy(laptop)
 	if err != nil {
-		return fmt.Errorf("Couldn't copy laptop data: %w", err)
+		return err
 	}
 
 	m.data[laptop.Id] = other
@@ -59,11 +61,56 @@ func (m *MemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 	}
 
 	// deep copy
+	return deepCopy(laptop)
+
+}
+
+func (m *MemoryLaptopStore) Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
+
+	for _, laptop := range m.data {
+		if isQualified(filter, laptop) {
+			// TODO
+		}
+	}
+
+	return nil
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 	other := &pb.Laptop{}
 	err := copier.Copy(other, laptop)
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't copy laptop data: %w", err)
 	}
-
 	return other, nil
+}
+
+// Determines if a laptop qualifies to be returned by searchLaptop filter.
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPrice() > filter.GetMaxPrice() || laptop.GetCpu().GetCores() < filter.GetMinCores() || laptop.GetCpu().MinGhz < filter.GetMinGhz() || toBit(laptop.GetMemory()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+	return true
+}
+
+// converts memory to bit for comparing purposes.
+func toBit(memory *pb.Memory) uint64 {
+	val := memory.GetValue()
+
+	switch memory.GetUnit() {
+	case pb.Memory_BIT:
+		return val
+	case pb.Memory_BYTE:
+		return val << 3
+	case pb.Memory_MEGABYTE:
+		return val << 23
+	case pb.Memory_GIGABYTE:
+		return val << 33
+	case pb.Memory_TERABYTE:
+		return val << 43
+	default:
+		return 0
+	}
 }
